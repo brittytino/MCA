778d6843e73b722e33cf493d824614bbc85c1b15
brittytino <brittytino08@gmail.com>
2025-10-12 10:08:28 +0530

    web lab 9 added

---
+++ Programs/Practice-Java/Conditionals.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+class Conditionals {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number: ");
+        // int n = 5;
+
+        // while(n<10){
+        //     n=n+1;
+        //                 System.out.println("Number is less than 10, current number: " + n);
+
+        // }
+        
+int n = 1;
+do{
+    n++;
+    System.out.println("Number : " + n);
+}
+while(n<=10);{
+    n=n+1;
+    System.out.println("Number is less than 10, current number: " + n);
+}
+
+        // System.out.println("Your Number: "+ a);
+
+        // for(int i=1;i<=n;i++){
+        //     System.out.println(i);
+        // }
+
+        // if (n<10){
+        //     n=n+10;
+        // }
+        // else{
+        //     n=0;
+        // }
+
+// switch (n) {
+//     case 1:
+//     if (n<5) {
+//         System.out.println("less than 5");
+//     }
+//     else{
+//         System.out.println("not less than 5");
+//     }
+        
+//     case 2:
+//     if(n>5 && n<10){
+//         System.out.println("less than 10");
+//     }
+//     else
+//     {
+//         System.out.println("not less than 10");
+//     }
+//     case 3:
+//     if(n>100){
+//         System.out.println("greater than 100");
+//     }
+//     else{
+//         System.out.println("not greater than 100");
+//     }
+
+//     default:
+//         break;
+// }
+
+        // System.out.println(n);
+
+    }
+}
