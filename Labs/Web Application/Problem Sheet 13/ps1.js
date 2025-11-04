ce8f786ea9b9b4550ac5b8465f929bc1facda576
brittytino <brittytino08@gmail.com>
2025-11-04 19:09:42 +0530

    new

---
+++ Labs/Web Application/Problem Sheet 13/ps1.js
@@ -0,0 +1,14 @@
+// let timer = setInterval(function() {
+//  count++;
+//  console.log(count);
+//  if (count === 5) {
+//  clearInterval(timer);
+//  }
+// }, 1000);
+// let count = 0;
+
+
+console.log("Start");
+setTimeout(function() {
+ console.log("Inside Timeout");
+}, 3000);
